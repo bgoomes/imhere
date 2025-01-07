84c00c50246b5e25da4d545984db41687b2375c0
Bruno Gomes <obrunogomes@obrunogomes.local>
2025-01-07 12:31:26 -0300

    add src

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from "react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ola mundo!</Text>
-      <StatusBar style="auto" />
+      <Text key='1' style={styles.eventName}>Nome do evento</Text>
+      <Text key='2' style={styles.eventDate}>Terça, 06 de Janeiro de 2025</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
+const styles =  StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#131016',
+    padding: 24,
   },
-});
+  eventName: {
+    color: '#fdfcfe',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+  eventDate: {
+    color: '#6b6b6b',
+    fontSize: 16,
+  },
+})
